bfdd29ab629cbdf63b42669b4fc74ca242e9d40d
Sharat M R <cosmosgenius@gmail.com>
2015-01-25 19:19:38 +0530

    added initial configs

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+"use strict";
+
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        env : {
+            dev : {
+                NODE_ENV : "development",
+                DEBUG: "webhooked:*"
+            },
+            test : {
+                NODE_ENV : "test",
+            },
+            prod : {
+                NODE_ENV : "production"
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    globals: ["should"],
+                    timeout: 10000,
+                    ignoreLeaks: false,
+                    ui: "bdd",
+                    reporter: "spec",
+                    require: ["should", "coverage/blanket"]
+                },
+                src: ["tests/**/*.js"]
+            },
+            "html-cov": {
+                options: {
+                    reporter: "html-cov",
+                    quiet: true,
+                    captureFile: "coverage.html"
+                },
+                src: ["tests/**/*.js"]
+            },
+            "travis-cov": {
+                options: {
+                    reporter: "travis-cov"
+                },
+                src: ["tests/**/*.js"]
+            },
+            "lcov": {
+                options:  {
+                    reporter: "mocha-lcov-reporter",
+                    quiet: true,
+                    captureFile: "lcov.info"
+                },
+                src: ["tests/**/*.js"]
+            }
+        },
+        watch: {
+            express: {
+                files: ["**/*.js","!**/node_modules/**"],
+                tasks: [],
+                options: {
+                    nospawn: true //Without this option specified express won"t be reloaded
+                }
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-mocha-test");
+    grunt.loadNpmTasks("grunt-env");
+
+    // Default task.
+    grunt.registerTask("default", ["env:dev", "watch"]);
+    grunt.registerTask("integration",["env:test"]);
+    grunt.registerTask("unit",["env:test", "mochaTest"]);
+    grunt.registerTask("test", ["unit","integration"]);
+};
